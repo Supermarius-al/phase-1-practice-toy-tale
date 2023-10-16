8e1dcffc9902b978d58e5e04ca8973566228eda7
Marius Alban <malban298@gmail.com>
2023-10-16 15:46:39 -0300

    getting there

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {fetchAllToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +13,67 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchAllToys(){
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(toyArr => toyArr.forEach(createCards))
+}
+document.querySelector(".add-toy-form").addEventListener('submit', submitToy)
+function submitToy(e){
+  e.preventDefault()
+  let toyObject = {
+    name: e.target.name.value,
+    image: e.target.image.value,
+    likes: 0
+  }
+ 
+  addNewToy(toyObject)
+  createCards(toyObject)
+}
+
+function addNewToy(toyObject){
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toyObject)
+  })
+  .then(res => res.json())
+}
+
+
+function createCards(toy){
+  let card = document.createElement('div')
+  card.className = "card"
+  card.innerHTML = `
+  <img src="${toy.image}" class="toy-avatar">
+  <div class="content">
+    <h2>${toy.name}</h2>
+    <p>${toy.likes}</p>
+    <button class="like-btn" id="[toy_id]">Like ❤️</button>
+  `
+card.querySelector(".like-btn").addEventListener("click", () => {
+toy.likes++
+card.querySelector('p').textContent = toy.likes
+updateLike(toy)
+})
+  let toyCollection = document.querySelector("#toy-collection")
+
+  toyCollection.appendChild(card)
+  
+} 
+
+// Count likes
+
+function updateLike(toyObject){
+  fetch(`http://localhost:3000/toys/${toyObject.id}`,{
+    method: 'PATCH',
+    headers: {'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(toyObject)
+  }
+  ).then(res => res.json())
+  .then(animal => console.log(animal))
+}
